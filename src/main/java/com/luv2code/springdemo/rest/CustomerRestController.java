500e4e689f6251754ae5d8c31fd9c6e70aa29827
alenagrrr <alena.savachenko@student.hu.nl>
2020-08-25 17:39:09 +0200

    add new customer via rest api

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -3,10 +3,7 @@ package com.luv2code.springdemo.rest;
 import com.luv2code.springdemo.entity.Customer;
 import com.luv2code.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,4 +31,18 @@ public class CustomerRestController {
         }
         return  theCustomer;
     }
+
+    //add mapping for post /customers  adding new customer
+
+    @PostMapping("/customers")
+
+    public Customer addCustomer (@RequestBody Customer theCustomer)
+    {
+        //if the id is 0, than dao wil insert new customer
+        theCustomer.setId(0);
+        customerService.saveCustomer(theCustomer);
+        return theCustomer;
+    }
+
+
 }
